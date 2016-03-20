0da784478a0cfe6bd74029c2bed5920fb8d1f7ba
arnoldlcl <arnoldlindlau@gmail.com>
2016-03-20 15:22:25 -0400

    changed split.R to be less dependent on the data directory

--- lib/split.R
+++ lib/split.R
@@ -1,6 +1,3 @@
 set.seed(88882)
 
-data_dir <- "./output/features"
-feature_names <- list.files(data_dir, pattern = "rds")
-
-ttsplit <- sample.int(length(feature_names), 5000)
+ttsplit <- sample.int(nrow(catdog), 5000)
