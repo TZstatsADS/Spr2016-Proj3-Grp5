4f3ce042fbd2c4db43c69a32766f1e7cdae1e9ce
arnoldlcl <arnoldlindlau@gmail.com>
2016-03-21 15:41:34 -0400

    added code to test.R to use baseline predictions in place of final predictions for observations with missing features

--- lib/test.R
+++ lib/test.R
@@ -9,9 +9,15 @@ test <- function(fit_train, dat_test) {
   ### Output: a list of 2; first element is a vector of class label predictions for the BASELINE model,
   ###                      second element is a vector of class label predictions for the FINAL model
   
+  NA_indices <- which(!complete.cases(dat_test))
+  
   pred_baseline <- predict(fit_train[[1]], newdata = dat_test[, 1:125])
   if (!is.na(fit_train[[2]])) {
     pred_final <- predict(fit_train[[2]], newdata = dat_test[, 126:1290])
+    # insert baseline predictions for observations that include NA features
+    for (i in 1:length(NA_indices)) {
+      pred_final <- append(pred_final, pred_baseline[NA_indices[i]], after = NA_indices[i] - 1)
+    }
   } else {pred_final <- NA}
   
   output <- list(pred_baseline, pred_final)
