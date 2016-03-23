a301392fc28a9c332a3a54d7f61f348e020cea18
arnoldlcl <arnoldlindlau@gmail.com>
2016-03-22 22:56:13 -0400

    fixed typo in test.R

--- lib/test.R
+++ lib/test.R
@@ -13,7 +13,7 @@ test <- function(fit_train, dat_test) {
   NA_indices <- which(!complete.cases(dat_test))
   
   pred_baseline <- predict(fit_train[[1]], newdata = dat_test[, 1:125])
-  pred_final <- predict(fit_train[[2]], newdata = dat_test[, 126:1290], type = "vector")
+  pred_final <- predict(fit_train[[2]], newdata = as.data.frame(dat_test[, 126:1290]), type = "vector")
   # insert baseline predictions for observations that include NA features
   for (i in 1:length(NA_indices)) {
     pred_final <- append(pred_final, pred_baseline[NA_indices[i]], after = NA_indices[i] - 1)
